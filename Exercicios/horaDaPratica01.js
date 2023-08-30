63918a1838d3693b8e7445b523f189c336a1e33e
Luis Felipe Ribeiro <44618496+felipert2@users.noreply.github.com>
2023-08-30 18:56:46 -0300

    primeiro commit

---
+++ Exercicios/horaDaPratica01.js
@@ -0,0 +1,153 @@
+// alert('Boas vindas ao nosso site !');
+// let = "lua";
+// let = "Idade";
+// let  idade = 25;
+// let numeroDeVendas = 50;
+// let saldoDisponivel = 1000;
+// alert ("Erro! Preencha todos os campos.");
+// let mensagemDeErro = ' Erro! Preencha todos os campos.'
+// alert (mensagemDeErro)
+// let nome =  prompt('Digite o seu nome ?');
+// let idade2 = prompt('Digite sua idade !')
+// if (idade2 >= 18 ){
+//     alert ('Pode tirar a Habilitação')
+// } 
+
+// diaDaSemana = prompt("Qual o dia da semana ? ")
+// if(diaDaSemana == "Sabado" || diaDaSemana == "Domingo"){
+//     alert("Bom fim de semana !")
+//     }else{
+//         alert("Boa Semana")
+//     }
+
+// let numeroUsuario = prompt("Digite o numero: ")
+// if(numeroUsuario > 0){
+//     alert("Numero Positivo")    
+// }else {
+//     alert("Numero Negativo")
+// }
+
+//     let pontuacao = prompt("Digite os Pontos")
+//     if(pontuacao < 100){
+//         alert("Jogue melhor")
+//     }else{
+//         alert("Campeeeaooooo")
+//  }
+
+// let saldo = 1500
+// alert("Seu Saldo é: " + saldo  )
+
+// nome = prompt("Digite o Seu nome: ")
+// alert("Bem vindo " + nome)
+
+
+// let contador3 = 1;
+
+// while(contador3 <= 10 ){
+//     console.log(contador3)
+//      contador3++
+// }
+
+
+
+// let contador4 = 10;
+
+// while(contador4 > 0){
+//     console.log(contador4)
+//      contador4--
+// }
+
+
+
+
+// let numero5 = prompt("digite o seu numero");
+// while(numero5 > 0){
+//     console.log(numero5)   
+//     numero5--
+// }
+
+// let numero6 = prompt ("digite o valor");
+// let contador = 0;
+
+// while (contador <= numero6){
+// console.log(contador);
+// contador++
+// }
+
+// //Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.
+// let boas = "Seja Bem Vindo"
+// console.log(boas)
+
+// //Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
+// let nome = "Felipe";
+// console.log("Olá " + nome)
+
+// //Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .
+// let nome1 = "Felipe" ;
+// alert("Olá " + nome1)
+
+// //Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+// let aviso = prompt("Qual a linguagem de programação que você mais gosta?")
+// console.log(aviso)
+
+// //Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a soma desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A soma de [valor1] e [valor2] é igual a [resultado]." no console.
+// let valor1 = 2;
+// let valor2 = 3;
+// let resultado = valor1 + valor2;
+// console.log(resultado)
+
+// //Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a subtração desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A diferença entre [valor1] e [valor2] é igual a [resultado]." no console.
+// let valor3 = 2;
+// let valor4 = 3;
+// let resultado2 = valor3 - valor4;
+// console.log(resultado2)
+
+
+// //Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a pessoa é maior ou menor de idade, exibindo uma mensagem apropriada no console.
+// let idade = prompt("Qual é a sua idade");
+// if(idade >= 18 )
+// {
+// console.log("É maior de idade");
+// }else{
+//     console.log("É menor de idade");
+// }
+
+
+// //Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
+// let numero = prompt ("Digite um numero");
+// if(numero > 0){
+//     console.log(numero + " É positivo")
+// }else{
+//     if(numero < 0){
+//         console.log(numero + " É negativo")
+//     }else{
+//         console.log("Numero é igual a zero")
+//     }
+// }
+
+// //Use um loop while para imprimir os números de 1 a 10 no console.
+// let contador = 1; 
+// while(contador <= 10){
+//     console.log(contador)
+//     contador++
+// }
+
+// //Crie uma variável "nota" e atribua um valor numérico a ela. Use if-else para determinar se a nota é maior ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console
+// let nota = 6;
+// if(nota > 7){
+//     console.log("Aprovado")
+// }else{
+//     console.log("Reprovado")
+// }
+
+// //Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
+// let random =  Math.random();
+// console.log(random)
+
+// //Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
+// let random2 = parseInt(Math.random()* 10 +1 )
+// console.log(random2)
+
+// //Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
+// let random2 = parseInt(Math.random()* 1000 +1 )
+// console.log(random2)
